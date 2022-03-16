b98b724d36ded0af120ba9c646860427835727d5
Hycient Onyeukwu <onyeukwuhycient@gmail.com>
2022-03-16 14:19:58 +0100

    added attribution

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -10,7 +10,10 @@ export default function Footer(){
 
   return(
     <footer className="footer-container">
-      <p className="title">Invoicebiz @ 2021. All rights reserved</p>
+      <div className="">
+        <p className="title"><a href="http://hycient-portfolio.herokuapp.com" target="_blank">Hycient Onyeukwu</a> | 2022</p>
+        <p>Design concept by <a href="https://www.linkedin.com/in/divine-chukwudi-bb9b416b" target="_blank">Divine</a></p>
+      </div>
       <ul>
         <li><a href="#">HOME</a></li>
         <li><a href="">COLLECTIVE</a></li>
