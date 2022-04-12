b8a2e463a59bca04f1c4f45bf2a3a0fe6bd57657
Hycient195 <onyeukwuhycient@gmail.com>
2022-04-12 12:32:52 +0100

    Total refactor

---
+++ schema.js
@@ -0,0 +1,35 @@
+user = {
+  first_name,
+  last_name,
+  avatar,
+  email,
+  phone_number,
+  timestamp
+}
+
+feed = {
+  category,
+  time_generated,
+  user_id,
+  timestamp
+}
+
+account = {
+  balance,
+  overdue_amount,
+  owing_amount,
+  invoiced_amount,
+  amount_paid,
+  user_id,
+  timestamp
+}
+
+invoices = {
+  client_name,
+  issue_date,
+  due_date,
+  amount,
+  status,
+  user_id,
+  timestamp
+}
